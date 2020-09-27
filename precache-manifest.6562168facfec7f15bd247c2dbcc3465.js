b856da343419c60a5bd131def7c54f708212db42
Stephan Ahlf <stephan.ahlf@googlemail.com>
2020-09-27 12:13:57 +0200

    add build folder for deployment to gh-pages branch

---
+++ precache-manifest.6562168facfec7f15bd247c2dbcc3465.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2822cf1a34de25e45e2b8444784a0ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa6ee919f136b58cfc9b",
+    "url": "/static/css/2.37e4066a.chunk.css"
+  },
+  {
+    "revision": "d431d67ac9942a35e9b9",
+    "url": "/static/css/main.a5f23165.chunk.css"
+  },
+  {
+    "revision": "aa6ee919f136b58cfc9b",
+    "url": "/static/js/2.bc4e3b2f.chunk.js"
+  },
+  {
+    "revision": "a1c44227e4d726a981b6ac8cf5c94401",
+    "url": "/static/js/2.bc4e3b2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d431d67ac9942a35e9b9",
+    "url": "/static/js/main.276875a9.chunk.js"
+  },
+  {
+    "revision": "87b8f2fc93bd8385fdf7",
+    "url": "/static/js/runtime-main.8503e087.js"
+  },
+  {
+    "revision": "8a6bd56a8d71b44673a36c25e1ca66aa",
+    "url": "/static/media/bB.8a6bd56a.svg"
+  },
+  {
+    "revision": "47af274b8a88eec771681b9ac53dc6d2",
+    "url": "/static/media/bK.47af274b.svg"
+  },
+  {
+    "revision": "0026f14668177f7dd0d4bab167b1a1cf",
+    "url": "/static/media/bN.0026f146.svg"
+  },
+  {
+    "revision": "344e161b311ce904cc0e3be7d7d61090",
+    "url": "/static/media/bP.344e161b.svg"
+  },
+  {
+    "revision": "aed026d0021b7ebfc2164946c0136010",
+    "url": "/static/media/bQ.aed026d0.svg"
+  },
+  {
+    "revision": "027332a07c4f25930a0e527a85965aa4",
+    "url": "/static/media/bR.027332a0.svg"
+  },
+  {
+    "revision": "970550187aa320d378080ae003b365c1",
+    "url": "/static/media/brown.97055018.svg"
+  },
+  {
+    "revision": "696db320e0177aab88116da43788fc44",
+    "url": "/static/media/logo.696db320.svg"
+  },
+  {
+    "revision": "e0eb761a13389f391cf3e6f643597644",
+    "url": "/static/media/wB.e0eb761a.svg"
+  },
+  {
+    "revision": "bb430c603fae05171bfcc6760efda928",
+    "url": "/static/media/wK.bb430c60.svg"
+  },
+  {
+    "revision": "f9c6e522d506c7c3a555e412d34630df",
+    "url": "/static/media/wN.f9c6e522.svg"
+  },
+  {
+    "revision": "4ff1ce7c9541a77b98582a3329e920ed",
+    "url": "/static/media/wP.4ff1ce7c.svg"
+  },
+  {
+    "revision": "4e4432a6b0dfce093177db46dc9bff48",
+    "url": "/static/media/wQ.4e4432a6.svg"
+  },
+  {
+    "revision": "573501233a407695e7b5d5b8e6efc7aa",
+    "url": "/static/media/wR.57350123.svg"
+  }
+]);
